321c25926e46de5a3a03f459f5409bfc2f9cf0d9
MAICK <maick298@gmail.com>
2024-12-12 23:54:29 +0100

    Web scrapping, open collection page, and click videos

--- Tests.py
+++ Tests.py
@@ -1,16 +1,13 @@
 import time
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-
-#Open the page in a chrome already open
-#First is necessary open chrome from the terminal: chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\Selenium"
-#Put the port 9222 and indicate that the user c:selenium will be the instance going to use
-chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")  # Connect to depurate's port
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def ScrollPage(scroll_pause_time):
-    #get the screen_height
+    #get the screen_height tes 
     screen_height = driver.execute_script("return window.screen.height;")
     i = 1
     print("Scrolling page...")
@@ -23,24 +20,59 @@ def ScrollPage(scroll_pause_time):
         if (screen_height) * i > scroll_height:
             break
 
-link = "https://www.tiktok.com/@maickmos"
-#driver = webdriver.Chrome()
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(link)
-time.sleep(10)
+def Click(class_element):
+    try:
+        print("Doing Click in button collection")
+        element = driver.find_element(By.CSS_SELECTOR, class_element)
+        print("Waiting that existed the element")
+        WebDriverWait(driver,10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR,class_element))
+        )
+        element.click()
+        print("click")
+        
+    except Exception as error:
+        print(f"Error en: {error}")
 
-try:
-    print("Doing Click in button Favorite")
-    button_favorate = driver.find_element(By.CLASS_NAME, "css-1wncxfu-PFavorite e1jjp0pq3")
-    button_favorate.click()
-    time.sleep(10)
-    print("Doing Click in button collection")
-    button_favorate = driver.find_element(By.CLASS_NAME, "TUXButton-content")
-    button_favorate.click()
+def Getlinkstiktoksfrompage(className):
+    print("Getting links of videos...")
+    script  = "let l = []; "
+    script += "Array.from(document.getElementsByClassName(\"" +className + "\")).forEach(item => { "
+    script += "    let link = item.querySelector('a'); "
+    script += "    if (link) { l.push(link.href); } "
+    script += "}); "
+    script += "return l;"
+
+    wait = WebDriverWait(driver, 10)  # Espera hasta 10 segundos
+    if className[0] == ".":
+        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,className.replace(" ","."))))
+    else:
+        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "."+className.replace(" ","."))))
     
-except Exception as error:
-    print("Error en: ")
+    urlsToDownload = driver.execute_script(script)
+    print(f"Found {len(urlsToDownload)} videos")
+    return urlsToDownload
+
+link_home_page_tiktok = "https://www.tiktok.com/@maickmos"
+class_button_favorite = "css-1wncxfu-PFavorite e1jjp0pq3"
+class_collection = "css-13fa1gi-DivWrapper e1cg0wnj1"
+class_tiktok_video = "css-8dx572-DivContainer-StyledDivContainerV2 eq741c50"
+
+#driver = webdriver.Chrome()
+#Open the page in a chrome already open
+#First is necessary open chrome from the terminal: r"
+#Put the port 9222 and indicate that the user c:selenium will be the instance going to use
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")  # Connect to depurate's port
 
 
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(link_home_page_tiktok)
+Click("."+class_button_favorite.replace(" ","."))
+collections = Getlinkstiktoksfrompage(class_collection)
+print(collections)
+#links = Getlinkstiktoksfrompage(class_tiktok_video)
+#print(links)
 
-input("Presiona Enter para continuar...")
+print("Esperando para cerrar...")
+time.sleep(60)
